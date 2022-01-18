b290e86be8fa56118645f1f957a5387020c37cdd
Sid Arutla <sidarutla@gmail.com>
2022-01-17 17:20:48 -0800

    intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Attachments/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Attachments/Intake.java
@@ -2,39 +2,35 @@ package org.firstinspires.ftc.teamcode.Attachments;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 
 public class Intake {
-    CRServo intakeServo1;
-    CRServo intakeServo2;
+    DcMotor intakeMotor;
     private LinearOpMode op;
 
     public Intake(LinearOpMode opMode) {
         this.op = opMode;
-        intakeServo1 = op.hardwareMap.crservo.get("IntakeServo1");
-        intakeServo2 = op.hardwareMap.crservo.get("IntakeServo2");
+        intakeMotor = op.hardwareMap.dcMotor.get("IntakeMotor");
     }
 
     public void turnIntakeForward(boolean intakeOn) {
         if (intakeOn) {
             op.telemetry.addData("setting power", null);
             op.telemetry.update();
-            intakeServo1.setPower(-1);
-            intakeServo2.setPower(1);
+            intakeMotor.setPower(0.5);
         }
     }
 
     public void turnIntakeBackward(boolean intakeOn) {
         if (intakeOn) {
-            intakeServo1.setPower(1);
-            intakeServo2.setPower(-1);
+            intakeMotor.setPower(-0.5);
         }
     }
     public void stopIntake(boolean forwardOff, boolean backwardOff) {
         if (!forwardOff && !backwardOff) {
-            intakeServo1.setPower(0);
-            intakeServo2.setPower(0);
+            intakeMotor.setPower(0);
         }
     }
  }
