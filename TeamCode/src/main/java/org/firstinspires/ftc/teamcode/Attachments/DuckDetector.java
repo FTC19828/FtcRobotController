492f44812e0e9a3527cd84ae144bf3bb519496dd
AG343 <80183838+AG343@users.noreply.github.com>
2022-03-21 18:25:23 -0700

    Update DuckDetector.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Attachments/DuckDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Attachments/DuckDetector.java
@@ -14,16 +14,16 @@ public class DuckDetector {
     private LinearOpMode op;
     private static final String TFOD_MODEL_ASSET = "RubiksCubeModel.tflite";
     private static final String[] LABELS = {
-          "Rubiks Cube"
+            "Rubiks Cube"
     };
     private static final String VUFORIA_KEY =
-"AS+3miP/////AAABmUADHX1oj0Mrgz+nUhz+Kx4of+OMAHHpc1WO+qVoM6oFVjzjDmTHl+/HKvX4UBJfzOJ1czBlCzfaK9H5/+YYapP0vJq0N+0OnUvQmow/94xqEY+cciWJzxZ+yCm+jwxeaTSjkSwu1tRmkwoEXq06R4l8JHtaOMOpdOsbHHdxg0vql+yny2q4C9yybqz2sxvzbF+9J2B8LcNArikLcNTkXDJKXlbdk4dm74ocRjzl4grUIq89MbB/AnWXLCO54+FQlnazS+Ro4FvUfpxIDzkGydcCgJDLRJaH5Zh1rY6QumUGhPcxDLHNtiLRdZWXhRhPvk732UwHUO/8AjMTpr5cxyVvkyZTmNTNGydNBE3CqGRv";   
+            "AS+3miP/////AAABmUADHX1oj0Mrgz+nUhz+Kx4of+OMAHHpc1WO+qVoM6oFVjzjDmTHl+/HKvX4UBJfzOJ1czBlCzfaK9H5/+YYapP0vJq0N+0OnUvQmow/94xqEY+cciWJzxZ+yCm+jwxeaTSjkSwu1tRmkwoEXq06R4l8JHtaOMOpdOsbHHdxg0vql+yny2q4C9yybqz2sxvzbF+9J2B8LcNArikLcNTkXDJKXlbdk4dm74ocRjzl4grUIq89MbB/AnWXLCO54+FQlnazS+Ro4FvUfpxIDzkGydcCgJDLRJaH5Zh1rY6QumUGhPcxDLHNtiLRdZWXhRhPvk732UwHUO/8AjMTpr5cxyVvkyZTmNTNGydNBE3CqGRv";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
     public DuckDetector(LinearOpMode opMode){
         this.op=opMode;
-       initVuforia();
-       initTfod();
+        initVuforia();
+        initTfod();
         if (tfod != null) {
             tfod.activate();
 
@@ -42,60 +42,62 @@ public class DuckDetector {
     }
 
     public int getDuckLocation(){
-       int num = 0;
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    // the last time that call was made.
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-                        op.telemetry.addData("# Object Detected", updatedRecognitions.size());
-                        // step through the list of recognitions and display boundary info.
-                        int i = 0;
-                        boolean isDuckDetected=false;
-                        for (Recognition recognition : updatedRecognitions) {
-                            op.telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                            op.telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                    recognition.getLeft(), recognition.getTop());
-                            op.telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                    recognition.getRight(), recognition.getBottom());
-                            i++;
+        int num = 0;
+        if (tfod != null) {
 
-                            if (recognition.getLabel().equals("Rubiks Cube")) {
-                                isDuckDetected = true;
-                                op.telemetry.addData("Object Detected", "Rubiks Cube");
-                                if(recognition.getLeft()>=400 && recognition.getLeft()<=700){
-                                    num=1;
-                                    op.telemetry.addData("1", null);
-                                    op.telemetry.update();
+            for (int i = 0; i < 5; i++) {
+                tfod.getUpdatedRecognitions();
+                op.sleep(250);
+            }
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                op.telemetry.addData("# Object Detected", updatedRecognitions.size());
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                boolean isDuckDetected=false;
+                for (Recognition recognition : updatedRecognitions) {
+                    op.telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                    op.telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                            recognition.getLeft(), recognition.getTop());
+                    op.telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                            recognition.getRight(), recognition.getBottom());
+                    i++;
 
-                                }
-                                else if(recognition.getLeft()>150 && recognition.getLeft()<=300){
-                                    num=2;
-                                    System.out.print("2");
-                                    op.telemetry.addData("2", null);
-                                    op.telemetry.update();
-                                }
-                                else if(recognition.getLeft()>=-100 && recognition.getLeft()<=100){
-                                     num=3;
-                                    System.out.print("3");
-                                    op.telemetry.addData("3", null);
-                                    op.telemetry.update();
-                                }
-                                else{
-                                    num=0;
-                                }
-                            } else {
-                                isDuckDetected = false;
-                                op.telemetry.addData("Rubiks Cube not Detected", null);
-                                op.telemetry.update();
-                            }
+                    if (recognition.getLabel().equals("Rubiks Cube")) {
+                        isDuckDetected = true;
+                        op.telemetry.addData("Object Detected", "Duck");
+                        op.telemetry.update();
+                        if(recognition.getLeft()>=400 && recognition.getLeft()<=700){
+                            num=3;
+                            op.telemetry.addData("3", null);
+                            op.telemetry.update();
 
                         }
-                        op.telemetry.update();
+                        else if(recognition.getLeft()>150 && recognition.getLeft()<=300){
+                            num=2;
+                            op.telemetry.addData("2", null);
+                            op.telemetry.update();
+                        }
+                        else if(recognition.getLeft()>=-100 && recognition.getLeft()<=100){
+                            num=1;
+                            op.telemetry.addData("1", null);
+                            op.telemetry.update();
+                        }
+                        else{
+                            num=0;
+                        }
+                    } else {
+                        isDuckDetected = false;
                     }
+
                 }
-        return num;
+                op.telemetry.update();
             }
+        }
+        return num;
+    }
     private void initVuforia() {
         /*
          * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
